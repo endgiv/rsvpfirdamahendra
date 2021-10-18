9ee4c8fd36d2d0712081eed6652eb00659593a28
endgiv <91960715+endgiv@users.noreply.github.com>
2021-10-18 08:22:28 +0800

    last

--- index.php
+++ index.php
@@ -13,25 +13,48 @@ function validate($data) {
 
 
 // Templating
-if(empty(($_GET['to'])))
-    { $receiver = ""; }
+if(empty(($_GET['t'])))
+    { 
+        $sesi = ""; 
+        $sesi_h = ""; 
+        $nama = ""; 
+        $readonly = ""; 
+    }
 else
-    { $receiver = ucwords($_GET["to"]);};
+    { 
+        $receiver = $_GET['t'];
+        $sqlSelect2 = "SELECT * FROM rsvp1 where token = '$receiver'";
+        $result2 = mysqli_query($link,$sqlSelect2);
+        if (! empty($result2)) {
+            foreach($result2 as $row2){
+                $sesi = 'Session ' . $row2['sesi'];
+                $nama = $row2['nama'];
+            };
+        };
+        $readonly = "readonly";
+        $sesi_array = array(
+            "Session 1" => "11.00-12.00 WIB",
+            "Session 2" => "12.00-13.00 WIB",
+            "Session 3" => "13.00-14.30 WIB",
+        );
+        $sesi_h = $sesi_array[$sesi];
+    };
 
-if(empty(($_GET['s'])))
-    { $session = ""; } 
-else 
-    { $session = "Session ".urlencode($_GET["s"]);};
+
+
+    
 
 $swap_var = array(
     "{WEDDING_NAME}" => "Firda &amp; Mahendra",
     "{WEDDING_DATE}" => "Saturday, 07 November 2021",
     "{WEDDING_VENUE}" => "Villa Nusantara Syariah, Malang",
     "{WEDDING_LOCATION}" => "Jl. Argobimo No.29, Krajan, Ketindan, Kec. Lawang, Malang, Jawa Timur",
-    "{WEDDING_TO}" => $receiver,
-    "{WEDDING_SESSION}" =>  $session,
+    "{WEDDING_TO}" => $nama,
+    "{WEDDING_SESSION}" =>  $sesi,
+    "{WEDDING_SESSION_H}" =>  $sesi_h,
     "{WEDDING_PARENTS1}" =>  'Sadfudji Hadijanto & Lelly Asmara Sari',
     "{WEDDING_PARENTS2}" =>  'Darmono & Farida Usman',
+    "{ATTR_READONLY}" =>  $readonly,
     
  );
 
--- rsvp.php
+++ rsvp.php
@@ -1,6 +1,11 @@
 <?php 
 // koneksi database
-include 'config.php';
+use Phppot\DataSource;
+
+require_once 'import/DataSource.php';
+$db = new DataSource();
+$conn = $db->getConnection();
+
 
 
 // menangkap data yang di kirim dari form
@@ -8,24 +13,36 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $wish = $_POST['wish'];
 $attend = $_POST['attend'];
-$sesi = substr($_POST['sesi'], -1);
+
+$sesi = "";
+if (isset($_POST['sesi'])) {
+$sesi = substr($_POST['sesi'], -1);}
+
+$sesi = "";
+if (isset($_POST['sesi'])) {
+$sesi = substr($_POST['sesi'], -1);}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //prepare insert
-        $sql = "INSERT INTO rsvp (name, email, wish, attend, sesi) VALUES (?, ?, ?, ?, ?)";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            //bind var
-            mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $wish, $attend, $sesi);
+            $sqlInsert = "REPLACE into rsvp1 (sesi,nama,email,wish,attend,token) values (?,?,?,?,?,?)";
+            $paramType = "isssss";
+            $paramArray = array(
+                $sesi,
+                $nama,
+                $email,
+                $wish,
+                $attend,
+                $token
+            );
+            $insertId = $db->insert($sqlInsert, $paramType, $paramArray);
             
-            //
-            if(mysqli_stmt_execute($stmt)){
-                //redirect
+            if (! empty($insertId)) {
                 echo 'sent';
             } else {
-                echo "Kayaknya ada yang salah";
+                $type = "error";
+                $message = "Problem in Importing CSV Data";
             }
         }
         
-    }
+    
 ?>
