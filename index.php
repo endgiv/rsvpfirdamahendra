c262a2574bab64a2e886a700f87dd3a9049f7c37
endgiv <91960715+endgiv@users.noreply.github.com>
2021-10-18 13:59:24 +0800

    mysql

--- index.php
+++ index.php
@@ -1,59 +1,58 @@
 <?php 
 
-//check config
-include 'config.php';
+use Phppot\DataSource;
 
-//Validasi Karakter
-function validate($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-  }
+require_once 'import\DataSource.php';
+$db = new DataSource();
+$conn = $db->getConnection();
 
-
-// Templating
-if(empty(($_GET['t'])))
-    { 
-        $sesi = ""; 
-        $sesi_h = ""; 
-        $nama = ""; 
-        $readonly = ""; 
-    }
-else
-    { 
-        $receiver = $_GET['t'];
-        $sqlSelect2 = "SELECT * FROM rsvp1 where token = '$receiver'";
-        $result2 = mysqli_query($link,$sqlSelect2);
-        if (! empty($result2)) {
-            foreach($result2 as $row2){
-                $sesi = 'Session ' . $row2['sesi'];
-                $nama = $row2['nama'];
-            };
+if (!empty($_GET['t'])){
+$receiver = $_GET['t'];
+$sqlSelect = "SELECT * FROM rsvp1 where token='$receiver'";
+$result = $db->select($sqlSelect);
+    if (! empty($result)) { 
+        foreach($result as $row){
+            echo $row['token'];
+            echo $row['nama'];
+            $sesi = 'Session ' . $row['sesi'];
+                    $nama = $row['nama'];
+                    $readonly = "readonly";
+                    $sesi_array = array(
+                        "Session 1" => "11.00-12.00 WIB",
+                        "Session 2" => "12.00-13.00 WIB",
+                        "Session 3" => "13.00-14.30 WIB",
+                    );
+                    $sesi_h = $sesi_array[$sesi];
         };
-        $readonly = "readonly";
-        $sesi_array = array(
-            "Session 1" => "11.00-12.00 WIB",
-            "Session 2" => "12.00-13.00 WIB",
-            "Session 3" => "13.00-14.30 WIB",
-        );
-        $sesi_h = $sesi_array[$sesi];
-    };
 
-if(empty(($_GET['s'])))
-    { $session = ""; } 
-else 
-    { $session = "Session ".urlencode($_GET["s"]);};
+        $readonly = "readonly";
+    }
+    else
+    {
+        die('hehehehe');
+    };
+}
+else
+{
+    
+    $sesi = ""; 
+    $sesi_h = ""; 
+    $nama = ""; 
+    $readonly = ""; 
+    $receiver = ""; 
+};
 
 $swap_var = array(
     "{WEDDING_NAME}" => "Firda &amp; Mahendra",
     "{WEDDING_DATE}" => "Saturday, 07 November 2021",
     "{WEDDING_VENUE}" => "Villa Nusantara Syariah, Malang",
     "{WEDDING_LOCATION}" => "Jl. Argobimo No.29, Krajan, Ketindan, Kec. Lawang, Malang, Jawa Timur",
-    "{WEDDING_TO}" => $receiver,
-    "{WEDDING_SESSION}" =>  $session,
+    "{WEDDING_TO}" => $nama,
+    "{WEDDING_SESSION}" =>  $sesi,
     "{WEDDING_PARENTS1}" =>  'Sadfudji Hadijanto & Lelly Asmara Sari',
     "{WEDDING_PARENTS2}" =>  'Darmono & Farida Usman',
+    "{ATTR_READONLY}" =>  $readonly,
+    "{ATTR_SELECT}" =>  $readonly,
     
  );
 
